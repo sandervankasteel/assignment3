e80835ba158d698a92b83606b68aaaf9c127f19e
Sander van Kasteel <info@sandervankasteel>
2017-06-24 20:59:30 +0200

    Some stuff

---
+++ app/Models/Contract.php
@@ -0,0 +1,19 @@
+<?php
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Contract extends Model
+{
+
+    protected fillable = [
+        ''
+    ];
+
+    public function contractWeek(int $weekNumber) 
+    {
+        return null;
+    }
+
+}
+
